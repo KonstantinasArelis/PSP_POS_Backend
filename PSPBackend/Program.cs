246a68daa4e83777dff8e6c7602efe861a3273b3
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2024-11-12 15:21:36 +0200

    updated Program.cs to include User repo and service

--- PSPBackend/Program.cs
+++ PSPBackend/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddTransient<OrderRepository>();
 builder.Services.AddTransient<OrderService>();
 builder.Services.AddTransient<ReservationRepository>();
 builder.Services.AddTransient<ReservationService>();
+builder.Services.AddTransient<UserRepository>();
+builder.Services.AddTransient<UserService>();
 
 var app = builder.Build();
 
