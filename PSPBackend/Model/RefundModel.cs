537fc06210db20151937de3c7fe3cd58f335e943
KonstantinasArelis <arelis.konstantinas@gmail.com>
2024-12-04 19:57:36 +0200

    Updated refund model

--- PSPBackend/Model/RefundModel.cs
+++ PSPBackend/Model/RefundModel.cs
@@ -2,13 +2,13 @@ namespace PSPBackend.Model
 {
     public class RefundModel
     {
-        public int Id { get; set; }
-        public int BusinessId { get; set; }
-        public int OrderItemId { get; set; }
-        public bool ReturnedToInventory { get; set; }
-        public int RefundedQuantity { get; set; }
-        public decimal Amount { get; set; }
-        public string Reason { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public int id { get; set; }
+        public int business_id { get; set; }
+        public int order_item_id { get; set; }
+        public bool returned_to_inventory { get; set; }
+        public int refunded_quantity { get; set; }
+        public decimal amount { get; set; }
+        public string reason { get; set; }
+        public DateTime created_at { get; set; }
     }
 }
